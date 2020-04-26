80aaaca3c6f822a8c077b04e2555f9b72ece3421
liantengda <1666893831@qq.com>
2020-04-26 22:31:40 +0800

    spring的bean生命周期

--- src/main/java/com/lian/javareflect/springBeanTest/BeanLifeCycle.java
+++ src/main/java/com/lian/javareflect/springBeanTest/BeanLifeCycle.java
@@ -3,13 +3,16 @@ package com.lian.javareflect.springBeanTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.net.URL;
+
 public class BeanLifeCycle {
 
     public static void main(String[] args) {
 
-        System.out.println("现在开始初始化容器");
+        URL resource = BeanLifeCycle.class.getResource("");
 
-        ApplicationContext factory = new ClassPathXmlApplicationContext("C:\\util\\javaEngineer\\javaproject\\javareflect\\src\\main\\java\\com\\lian\\javareflect\\springBeanTest\\beans.xml");
+        System.out.println("现在开始初始化容器");
+        ApplicationContext factory = new ClassPathXmlApplicationContext("beans.xml");
         System.out.println("容器初始化成功");
         //得到Preson，并使用
         Person person = factory.getBean("person",Person.class);
